45754bb722fabefdb18f67ab4c32a41d24114bca
Dayitva <dayitvagoel@gmail.com>
2022-03-16 20:45:24 +0530

    first commit

--- parser/neural/nn.py
+++ parser/neural/nn.py
@@ -21,7 +21,8 @@ from __future__ import print_function
 import six
 
 import numpy as np
-import tensorflow as tf
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
 
 #***************************************************************
 def get_sizes(t):
